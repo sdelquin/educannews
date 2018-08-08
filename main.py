ad634ae951117ff84acf840897e1d93860a261a2
Sergio Delgado Quintero <sdelquin@gmail.com>
2018-08-08 21:15:50 +0100

    Add text cleaning in scrapped fields

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 result = requests.get('http://www.gobiernodecanarias.org/educacion/web/')
 c = result.content
 
 soup = BeautifulSoup(c, features='html.parser')
-form = soup.find('form', 'frm_bloque_novedades_categorizadas')
-content = form.parent
+content = soup.find('form', 'frm_bloque_novedades_categorizadas').parent
 all_news = content.find_all('div', 'noticia')
 for news in all_news:
     a = news.h3.a
     spans = a.find_all('span')
-    print(a['href'])
-    print(spans[0].string)
-    print(spans[1].string)
+    url = a['href'].strip()
+    date, category = (
+        t.strip() for t in
+        re.search(r'\[(.*)\].*\[(.*)\]', spans[0].string).groups()
+    )
+    title = spans[1].string.strip()
+    print(url, date, category, title, sep='\n')
