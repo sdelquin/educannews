0e413d177111457c5ee99ea570ba34a094a51b8a
Sergio Delgado Quintero <sdelquin@gmail.com>
2018-08-09 13:28:55 +0100

    Add better explanation on USER_IDS

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ MAX_NEWS_TO_SAVE_ON_DB = 100
 BOT_TOKEN = config('BOT_TOKEN', default='put here the token of your bot')
 USER_IDS = config(
     'USER_IDS',
-    default='put here your Telegram user id',
+    default='put here the user telegram ids who wants to receive news',
     cast=config.list
  )
