ceda07ec8f4a24e74a02ca2f0079c5250ede05b2
Sergio Delgado Quintero <sdelquin@gmail.com>
2018-08-10 11:22:32 +0100

    Remove Telegram chat IDs from config files since they are in database

--- config.py
+++ config.py
@@ -4,8 +4,3 @@ NEWS_URL = 'http://www.gobiernodecanarias.org/educacion/web/'
 DATABASE = 'news.db'
 MAX_NEWS_TO_SAVE_ON_DB = 100
 BOT_TOKEN = config('BOT_TOKEN', default='put here the token of your bot')
-USER_IDS = config(
-    'USER_IDS',
-    default='put here the user telegram ids who wants to receive news',
-    cast=config.list
- )
