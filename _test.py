dbb3d53eebbb0d61b4a061d43edf214132225ec5
Sergio Delgado Quintero <sdelquin@gmail.com>
2018-09-02 14:16:38 +0100

    Split tests

--- _test.py
+++ _test.py
@@ -9,20 +9,24 @@ TEST_DB_PATH = 'news.test.db'
 NUM_NEWS_TO_TEST = 3
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture
 def db_handlers():
     yield create_db(TEST_DB_PATH, force_delete=True, verbose=False)
-    # os.remove(TEST_DB_PATH)
+    os.remove(TEST_DB_PATH)
 
 
-def test_all(db_handlers):
-    dbconn, dbcur = db_handlers
-    educan_news = News(dbconn, dbcur)
-    # test telegram sending
+def test_telegram_sending(db_handlers):
+    educan_news = News(*db_handlers)
     educan_news.get_news(NUM_NEWS_TO_TEST)
     educan_news.dispatch_news()
     assert len(educan_news.news) == educan_news.num_news_on_db
-    # test telegram editing
+
+
+def test_telegram_editing(db_handlers):
+    dbconn, dbcur = db_handlers
+    educan_news = News(dbconn, dbcur)
+    educan_news.get_news(NUM_NEWS_TO_TEST)
+    educan_news.dispatch_news()
     newsitem = educan_news.news[0]
     dbcur.execute("update news set url='' where rowid=1")
     dbconn.commit()
@@ -30,7 +34,13 @@ def test_all(db_handlers):
     educan_news.dispatch_news()
     dbcur.execute("select * from news where rowid=1")
     assert newsitem.url == dbcur.fetchone()['url']
-    # test case insensitive messages
+
+
+def test_case_comparing(db_handlers):
+    dbconn, dbcur = db_handlers
+    educan_news = News(dbconn, dbcur)
+    educan_news.get_news(NUM_NEWS_TO_TEST)
+    educan_news.dispatch_news()
     newsitem = educan_news.news[0]
     dbcur.execute(
         f"update news set title='{newsitem.title.upper()}' where rowid=1"
