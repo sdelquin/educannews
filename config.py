9a3e36c042773cf78988189dbb66608d58aed5d3
Sergio Delgado Quintero <sdelquin@gmail.com>
2018-09-19 20:59:09 +0100

    Fix prettyconf casts

--- config.py
+++ config.py
@@ -8,13 +8,13 @@ DATABASE = config(
 )
 MAX_NEWS_TO_SAVE_ON_DB = config(
     'MAX_NEWS_TO_SAVE_ON_DB',
-    default='100',
-    cast=config.eval
+    default=100,
+    cast=int
 )
 ROUGH_NUM_NEWS_ON_FRONTPAGE = config(
     'ROUGH_NUM_NEWS_ON_FRONTPAGE',
-    default='10',
-    cast=config.eval
+    default=10,
+    cast=int
 )
 BOT_TOKEN = config(
     'BOT_TOKEN',
@@ -26,13 +26,13 @@ CHANNEL_NAME = config(
 )
 DELAY_BETWEEN_TELEGRAM_DELIVERIES = config(     # in seconds
     'DELAY_BETWEEN_TELEGRAM_DELIVERIES',
-    default='1',
-    cast=config.eval
+    default=1,
+    cast=float
 )
 NUM_TELEGRAM_RETRIES = config(
     'NUM_TELEGRAM_RETRIES',
-    default='3',
-    cast=config.eval
+    default=3,
+    cast=int
 )
 TELEGRAM_READ_TIMEOUT = config(
     'TELEGRAM_READ_TIMEOUT',
