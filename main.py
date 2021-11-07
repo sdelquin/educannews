9bcc9f51e78c1ee011f20a73efa8e10933216b30
Sergio Delgado Quintero <sdelquin@gmail.com>
2021-11-07 19:28:53 +0000

    Fix main script to use typer in cli handling

--- main.py
+++ main.py
@@ -1,23 +1,34 @@
-'''EduCanNews
-
-Usage:
-    main.py createdb
-    main.py notify
-'''
-from docopt import docopt
+import logzero
+import typer
 
 from core.db import create_db, init_db
 from core.news import News
 from core.utils import init_logger
 
 logger = init_logger()
+app = typer.Typer(add_completion=False)
 
-arguments = docopt(__doc__)
 
-if arguments['createdb']:
+@app.command()
+def createdb():
+    '''Create database.'''
     create_db(force_delete=False, verbose=False)
-elif arguments['notify']:
+
+
+@app.command()
+def notify(
+    verbose: bool = typer.Option(
+        False, '--verbose', '-v', show_default=False, help='Loglevel increased to debug'
+    )
+):
+    '''Get and dispatch news.'''
+    logger.setLevel(logzero.DEBUG if verbose else logzero.INFO)
+
     educan_news = News(*init_db())
     educan_news.get_news()
     educan_news.sift_news()
     educan_news.dispatch_news()
+
+
+if __name__ == "__main__":
+    app()
